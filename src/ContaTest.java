21f953c7900903799917a9d9be564eeafe6d54da
Wallace Stevan <wallaceste@hotmail.com>
2023-08-24 10:22:37 -0300

    Ajustes ContaTest

--- src/ContaTest.java
+++ src/ContaTest.java
@@ -5,31 +5,33 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContaTest {
-    Conta conta = new ContaCorrente(new Banco("BB", new int[]{1244, 4556}), 1244, "Wallace");
-    Conta conta2 = new ContaPoupanca(new Banco("ITAU", new int[]{1452, 2245}), 2245, "Wallace");
-
 
 
     @Test
     void depositar() {
-        conta.depositar(1000);
+        Conta conta = new ContaCorrente(new Banco("BB", new int[]{1244, 4556}), 1244, "Wallace");
+        conta.setSaldo(1000);
     }
 
     @Test
     void sacar() {
-        depositar();
+        Conta conta = new ContaCorrente(new Banco("BB", new int[]{1244, 4556}), 1244, "Wallace");
+        conta.setSaldo(1000);
         conta.sacar(150);
     }
 
     @Test
     void transferir() {
-        depositar();
+        Conta conta = new ContaCorrente(new Banco("BB", new int[]{1244, 4556}), 1244, "Wallace");
+        Conta conta2 = new ContaPoupanca(new Banco("ITAU", new int[]{1452, 2245}), 2245, "Wallace");
+        conta.setSaldo(1000);
         conta.transferir(500, conta2);
     }
 
     @Test
     void verSaldo() {
-        depositar();
+        Conta conta = new ContaCorrente(new Banco("BB", new int[]{1244, 4556}), 1244, "Wallace");
+        conta.setSaldo(1000);
         conta.verSaldo();
     }
 }
